83cab5eeeceea4a0b6b08f559868f95f5c1ff3d2
TORESTO <therealtoresto@gmail.com>
2020-07-12 12:08:48 +0300

    failed future

---
+++ 3-failed.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const future = executor => ({
+    chain(fn) {
+        return future((resolve, reject) => this.fork(
+            value => fn(value).fork(resolve),
+            error => reject(error),
+        ));
+    },
+    map(fn) {
+        return this.chain(value => future.of(fn(value)));
+    },
+    fork(successed, failed) {
+        executor(successed, failed);
+        return this;
+    }
+});
+
+future.of = value => future(resolve => resolve(value));
+
+// Usage
+
+future((resolve, reject) => reject(new Error('Rejected')))
+    .map(x => {
+        console.log('future started');
+        return x;
+    })
+    .map(x => ++x)
+    .map(x => x ** 3)
+    .fork(
+        value => {
+            console.log('future1 result', value);
+        }, 
+        error => {
+            console.log('future failed', error.message);
+        }
+    );
+
+// console.dir({ future });
